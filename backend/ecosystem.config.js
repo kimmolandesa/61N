12d2fcc54cc7d1b7c793423fa7aaa26693430cf1
Vahvaselka <vilkkuvahvaselka@gmail.com>
2026-05-16 12:29:13 +0300

    correction to config

--- backend/ecosystem.config.js
+++ backend/ecosystem.config.js
@@ -1,12 +1,14 @@
 module.exports = {
   apps: [{
-    name: 'n61-api',
-    script: '/root/n61/api/venv/bin/uvicorn',
+    name: '61N-api',
+    script: '/root/61N/api/venv/bin/uvicorn',
     args: 'main:app --host 0.0.0.0 --port 8000',
-    cwd: '/root/n61/api',
+    cwd: '/root/61N/api',
     interpreter: 'none',
     autorestart: true,
     watch: false,
     max_memory_restart: '1G'
   }]
 }
+
+
